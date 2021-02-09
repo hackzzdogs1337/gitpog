4eff9870d4d382f862ef0d01cf092af60bd57ca9
hackzzdogs1337 <anirudh.narayanan00@gmail.com>
2021-02-10 02:15:49 +0530

    Added repo based filtering

--- gitpog/main.py
+++ gitpog/main.py
@@ -12,6 +12,7 @@ filter=0
 deprecation_count=1
 def makerequest(word):
     result=[]
+    global presult
     global GITHUB_API_TOKEN
     global language
     equery=urllib.parse.quote('"'+presult.target+'" '+word.strip('\n'))
@@ -32,7 +33,36 @@ def makerequest(word):
             result.append(i['html_url'])
         deprecation_count+=1
     else:
-        print(response.text)
+        print(response.status_code)
+    #Filter results based on repos
+    if(presult.repo!=None and response.status_code==200):
+        filtered_links=[]
+        indices=[]
+        repo_names=[i['repository']['full_name'].split('/')[0] for i in response.json()["items"]]
+        for repo in repo_names:
+            if(presult.repo == repo):
+                    indices.append(repo_names.index(repo))
+        repo_results=[result[i] for i in indices]
+        if(presult.fu==None):
+            return(word,repo_results)
+        else:
+            result=repo_results
+    #Filter based on user of the target organisation    
+    '''if(presult.fu!=None and response.status_code==200):
+        indices=[]
+        users_url=[ i['repository']['owner']['url'] for i in response.json()["items"]]
+        #Make request to user api endpoint to get organisation name
+        userss_json=list(map(lambda x: requests.get(x,headers={'Authorization':'token '+token}).json()['company'],users_url))
+        users_json=[i.strip('@') for i in userss_json if i]
+
+        print(users_json)
+        for user_json in users_json:
+                if(user_json!=None):
+                    if(presult.target.lower()==user_json.lower()):
+                        indices.append(users_json.index(user_json))
+        user_filtered_results=[result[i] for i in indices]
+        return(word,user_filtered_results)'''
+        
     return (word,result)
 
 parser=optparse.OptionParser(description='Gitpog is a tool which tries to do github dorking')
@@ -42,7 +72,10 @@ parser.add_option('-l',dest='lang',help='Language to search (like ruby)')
 parser.add_option('-w',dest='wordlist',help='Wordlist containing a list of keywords',default='./wordlists/keywords.txt')
 parser.add_option('-k',dest='keyword',help='Specific keyword u want to search')
 parser.add_option('-o',dest='outputfile',help='Output to a file')
-parser.add_option('-r',dest='repo',help='Target github for better filtering of results')
+parser.add_option('-r',dest='repo',help='Filter by target home github repo')
+#Yet to be developed
+#parser.add_option('--userenum',dest='users',help='This flag will make the tool to search these keywords on people who work on the target organisation obtained from the initial gitpog result')
+#parser.add_option('-u',dest='fu',help='Filter the results based on people who work in the target organisation specify "y" if you want(this will take a bit of time:))')
 #parser result
 
 presult,dummy=parser.parse_args()
@@ -71,16 +104,8 @@ else:
     results=map(lambda x:tpool.submit(makerequest,x),f)
     for result in concurrent.futures.as_completed(results):
         word,links=result.result()
-        #Filter results based on repos
-        if presult.repo!=None:
-            filtered_links=[]
-            for link in links:
-                if(presult.repo in link):
-                    filtered_links.append(link)
-            links=filtered_links
-
         #word=word.strip('\n').strip('=').strip('-')
-        word=word.translate(word.maketrans('&=-\?$\n#','        ')).strip(' ')
+        word=word.translate(word.maketrans('&=-\?$\n','       ')).strip(' ')
         if links!=[]:
             print(colored(f'Results for the keyword {word}','green'))
             if(a==1 and links!=[]):
